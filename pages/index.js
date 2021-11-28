2ac08b65b7f4060798cedf20261532921716ef7e
David Paucar <soydavidpaucar@gmail.com>
2021-11-28 17:03:01 -0500

    :tada: spotify app
    created init project structure

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar.js';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Spotify 2.0</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			
+			<main>
+				{/* Sidebar */}
+				<Sidebar />
+				{/* Center */}
+			</main>
+			
+			<div>{/* Player */}</div>
+		</div>
+	);
+}
