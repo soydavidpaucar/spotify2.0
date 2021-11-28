9dcbcf17d2c9f38a7f0d9a81296eeefe9b85d619
David Paucar <soydavidpaucar@gmail.com>
2021-11-28 17:20:49 -0500

    :art: sidebar icons
    added sidebar icons to the structure

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,25 @@
+import { faSquarePlus } from '@fortawesome/pro-solid-svg-icons';
+import { faHouseBlank, faMagnifyingGlass, faBooks } from '@fortawesome/pro-thin-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Sidebar = () => {
 	return (
-		<div>Sidebar</div>
+		<div className="text-gray-500">
+			<div>
+				<button className="flex items-center space-x-2 hover:text-white">
+					<FontAwesomeIcon icon={faHouseBlank} className="h-5 w-5" />
+					<p>Home</p>
+				</button>
+				<button className="flex items-center space-x-2 hover:text-white">
+					<FontAwesomeIcon icon={faMagnifyingGlass} className="h-5 w-5" />
+					<p>Search</p>
+				</button>
+				<button className="flex items-center space-x-2 hover:text-white">
+					<FontAwesomeIcon icon={faBooks} className="h-5 w-5" />
+					<p>Your Library</p>
+				</button>
+			</div>
+		</div>
 	);
 };
 
