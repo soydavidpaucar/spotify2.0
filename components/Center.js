eb46d9ad77ff9b2f0d1aa63d87fa0c2716a875b0
David Paucar <soydavidpaucar@gmail.com>
2021-12-05 19:34:23 -0500

    :art: logout & songs list
    added logout option to center component and added songs list component wich render all songs from selected playlist

--- components/Center.js
+++ components/Center.js
@@ -1,11 +1,12 @@
 import { faChevronDown } from '@fortawesome/pro-solid-svg-icons';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import { shuffle } from 'lodash';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { playlistIdState, playlistState, } from '../atoms/playlistAtom.js';
 import useSpotify from '../hooks/useSpotify.js';
+import Songs from './Songs.js';
 
 const colors = [
 	'from-indigo-500',
@@ -37,10 +38,11 @@ const Center = () => {
 	console.log(playlist);
 	
 	return (
-		<div className="flex-grow">
+		<div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
 			<header className="absolute top-5 right-8">
 				<div
-					className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+					className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+					onClick={() => signOut()}>
 					<img className="rounded-full w-10 h-10" src={session?.user.image} alt="User Profile" />
 					<h2>{session?.user.name}</h2>
 					<FontAwesomeIcon icon={faChevronDown} className="h-5 w-5" />
@@ -49,8 +51,15 @@ const Center = () => {
 			
 			<section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}>
 				<img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="" />
-				<p>hello</p>
+				<div>
+					<p>PLAYLIST</p>
+					<h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+				</div>
 			</section>
+			
+			<div>
+				<Songs />
+			</div>
 		</div>
 	);
 };
