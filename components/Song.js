0f1290a53a621f8066b041a03163845b51e43e3e
David Paucar <soydavidpaucar@gmail.com>
2021-12-07 15:09:03 -0500

    :art: song component
    created song component listing all information about playlist and individual songs

--- components/Song.js
+++ components/Song.js
@@ -1,7 +1,37 @@
-const Song = () => {
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom.js';
+import useSpotify from '../hooks/useSpotify.js';
+import { millisToMinutesAndSeconds } from '../lib/time.js';
+
+const Song = ({ order, track }) => {
+	const spotifyApi = useSpotify();
+	const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+	const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+	
+	const playSong = () => {
+		setCurrentTrackId(track.track.id);
+		setIsPlaying(true);
+		spotifyApi.play({
+			uris: [track.track.uri]
+		});
+	};
 	return (
-		<div>
-		
+		<div className="grid grid-cols-2 text-gray-500 py-5 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+				 onClick={playSong}>
+			<div className="flex items-center space-x-4">
+				<p>{order + 1}</p>
+				<img className="h-10 w-10" src={track.track.album.images[0].url} alt={`${track.track.album.name} Album`} />
+				
+				<div>
+					<p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
+					<p className="w-40">{track.track.artists[0].name}</p>
+				</div>
+			</div>
+			
+			<div className="flex items-center justify-between ml-auto md:ml-0">
+				<p className="w-40 hidden md:inline">{track.track.album.name}</p>
+				<p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
+			</div>
 		</div>
 	);
 };
